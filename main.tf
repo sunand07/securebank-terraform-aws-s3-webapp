2309b08adb4e2a710a94c14fde2372d8fc2bd755
sunand07 <139065922+sunand07@users.noreply.github.com>
2025-10-22 13:42:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,78 +1,14 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-resource "aws_s3_bucket" "bucket" {
-  bucket_prefix = "${var.prefix}-${var.name}"
-
-  force_destroy = true
+variable "region" (
+description = "AS region"
+default= "ap-southeast-2"
 }
 
-resource "aws_s3_bucket_public_access_block" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+variable "instance_type"{
+description = Type of EC2 instance to provision"
+default= "t2.midfg"
 }
 
-resource "aws_s3_bucket_ownership_controls" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_website_configuration" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_acl" "bucket" {
-  depends_on = [
-    aws_s3_bucket_public_access_block.bucket,
-    aws_s3_bucket_ownership_controls.bucket,
-  ]
-  bucket = aws_s3_bucket.bucket.id
-
-  acl = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "policy" {
-  depends_on = [ aws_s3_bucket_acl.bucket ]
-  bucket = aws_s3_bucket.bucket.id
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_s3_object" "webapp" {
-  depends_on = [ aws_s3_bucket_acl.bucket ]
-  acl          = "public-read"
-  key          = "index.html"
-  bucket       = aws_s3_bucket.bucket.id
-  content      = file("${path.module}/assets/index.html")
-  content_type = "text/html"
+variable "instance name" (
+description = "EC2 instance name"
+default     ="Provisioned by Terraform"
 }
